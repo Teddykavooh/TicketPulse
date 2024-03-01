6ad1f21a73a3d13aa491f207a01ec75910559129
Teddykavooh <teddykavooh@gmail.com>
2024-03-01 19:53:35 +0300

    Add admin page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Events from "./components/Events";
 import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Event from "./components/Event";
+import Admin from "./components/Admin";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="login" element={<Login />} />
         <Route path="events/:id" element={<Event />} />
+        <Route path="admin" element={<Admin />} />
       </Route>
     </Routes>
   );
---
+++ src/components/Admin/index.js
@@ -0,0 +1,5 @@
+import "./index.scss";
+
+const Admin = () => {};
+
+export default Admin;
