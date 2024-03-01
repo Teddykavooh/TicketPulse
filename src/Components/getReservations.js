d9e708ff08d5e514488bcf857f40a250d53e5df0
Teddykavooh <teddykavooh@gmail.com>
2024-03-01 19:57:43 +0300

    Code clean-up

--- src/Components/getReservations.js
+++ src/Components/getReservations.js
@@ -7,15 +7,15 @@ const GetReservations = (req, res) => {
     "SELECT COALESCE(SUM(tickets.vipTickets + tickets.regularTickets), 0) AS totalReservations FROM tickets WHERE event = ? AND email = ?";
   db.query(getReservationsByQuery, [event_id, mail], (err, result) => {
     if (err) {
-      console.error("Error fetching reservations:", err);
+      // console.error("Error fetching reservations:", err);
       res.status(500).json({ error: "Error fetching reservations" });
     } else {
       const totalReservations =
         result.length > 0 ? result[0].totalReservations : 0;
 
-      console.log("Result: ", result);
-      console.log("totalReservations: ", totalReservations);
-      console.log("Query Parameters: ", [event_id, mail]);
+      // console.log("Result: ", result);
+      // console.log("totalReservations: ", totalReservations);
+      // console.log("Query Parameters: ", [event_id, mail]);
 
       res.status(200).json({ totalReservations });
     }
